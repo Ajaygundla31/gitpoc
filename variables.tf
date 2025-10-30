48b88ad07b02a83b6ce5a4e326343bac862abff0
Ajaygundla31 <ajaygundla31@gmail.com>
2025-10-30 17:35:48 +0530

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "vmname" {
+    description = "The name of Virtual Machine"
+  type        = string
+  default   = "Vm_test_Eu"
+  
+}
+variable "resource_group_name"{
+
+}
+
+variable "location"{
+
+}
+
+variable "subnet_id"{
+
+}
+
+variable "vmsku"{
+
+}
+variable "environment" {
+
+}
