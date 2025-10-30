3bb698023be916983fc3044698991bc29616fcc8
Ajaygundla31 <ajaygundla31@gmail.com>
2025-10-30 17:39:19 +0530

    Initial commit

--- Day2main.tf
+++ Day2main.tf
@@ -1,3 +1,44 @@
-terraform {
-    
+resource "azurerm_network_interface" "main" {
+  name                = "${var.vmname}-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "${var.vmname}-nic01"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "main" {
+  name                  = "${var.vmname}-vm"
+  location              = var.location
+  resource_group_name   = var.resource_group_name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  size               = var.vmsku
+  admin_username = "testadmin"
+  admin_password = "Password1234!"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+  os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+   storage_account_type = "Standard_LRS"
+  }
+
+
+  tags = {
+    environment = var.environment
+  }
 }
